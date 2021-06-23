b0c782515614ee6a37b939ed1827ce54848c354a
Jonas Elan <jonaselan@Jonass-MacBook-Pro.local>
2021-06-23 17:09:08 -0300

    create controller for student

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,25 @@
+package com.example.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/student")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+    @GetMapping
+    public List<Student> index() {
+        return studentService.getStudents();
+
+    }
+}
---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,16 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+    public List<Student> getStudents() {
+        return List.of(
+            new Student(1L, "maria", "email@email",LocalDate.of(2000, Month.JANUARY, 12), 21)
+        );
+    }
+}
