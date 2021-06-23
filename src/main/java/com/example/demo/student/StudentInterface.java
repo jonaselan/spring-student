fc12936c6ce76a74abc479936c4612611a97628b
Jonas Elan <jonas.elan@thoughtworks.com>
2021-06-23 18:21:04 -0300

    create entity JPA

--- src/main/java/com/example/demo/student/StudentInterface.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo.student;
-
-public interface StudentInterface {
-}
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,16 +1,21 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-            new Student(1L, "maria", "email@email",LocalDate.of(2000, Month.JANUARY, 12), 21)
-        );
+        return studentRepository.findAll();
     }
 }
