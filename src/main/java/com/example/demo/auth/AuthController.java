3f2b49691060e7d465fa42297d391ae767a93187
Jonas Elan <jonas.elan@gmail.com>
2021-06-30 17:03:00 -0300

    start to implement jwt authentication

---
+++ src/main/java/com/example/demo/auth/AuthController.java
@@ -0,0 +1,22 @@
+package com.example.demo.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+//    @PostMapping(path = "/login")
+//    public ResponseEntity<?> login(@RequestBody @Valid LoginData loginData) {
+//    }
+}
--- src/main/java/com/example/demo/config/security/SecurityConfig.java
+++ src/main/java/com/example/demo/config/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
@@ -24,9 +25,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/student")
+                .antMatchers(HttpMethod.POST, "/auth/login")
                 .permitAll()
                 .anyRequest().authenticated()
-        .and().formLogin();
+        .and().csrf().disable()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
